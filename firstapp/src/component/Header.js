a8ca6c374fe2d950d5472ecaf441cfbeff732731
priyanka chowdary <priyacherukuru2000@gmail.com>
2022-08-04 18:38:31 +0530

    state

--- firstapp/src/component/Header.js
+++ firstapp/src/component/Header.js
@@ -2,13 +2,32 @@ import React,{Fragment,Component} from 'react';
 import './Header.css'
 
 class Header extends Component {
+
+    constructor(){
+        super()
+
+        this.state={
+            title:"React Developer App",
+            keyword:'User Text Here'
+        }  
+
+        console.log(">>>>>inside constructor")
+    }
+
+    handleChange = (event) => {
+        console.log(event.target.value)
+        this.setState({keyword:event.target.value})
+    }
+    
+
     render(){
+        console.log(">>>>>inside render")
         return(
              <Fragment>
                 <header>
-                    <div className="logo">React App</div>
+                    <div className="logo">{this.state.title}</div>
                     <center>
-                        <input/>
+                        <input onChange={this.handleChange}/>
                         <div style={{color: 'white',fontSize:'20px'}}>User Test Here</div>
                     </center>
                 </header>
