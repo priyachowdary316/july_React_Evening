309a8e574732a211d38153c80d8afafb4c8a3567
priyanka chowdary <priyacherukuru2000@gmail.com>
2022-08-13 15:23:12 +0530

    routing6

--- routing5/src/component/PostDetails.js
+++ routing5/src/component/PostDetails.js
@@ -1,14 +1,17 @@
 import React from "react";
 
-const PostDetails = () => {
+const PostDetails = (props) => {
+    console.log(props.location.search.split('=')[1])
+    console.log(props.match.params.topic)
     return(
         <div className="panel panel-danger">
             <div className="panel-heading">
             PostDetails Page
             </div>
             <div className="panel-body">
-                Profile Page is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
-                Profile Page is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
+                {props.match.params.topic}Details Page is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
+                {props.match.params.topic}Profile Page is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
+                <h2>You are on page number {props.location.search.split('=')[1]}</h2>
         </div>
     </div>
     )
